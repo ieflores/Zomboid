aa4a38c4602b72f16a5bbfd6a6c2107e5aca53b4
Ivan Emmanuel Flores <ilflores@student.apc.edu.ph>
2023-07-04 20:55:33 +0800

    Color Scheme Changes

--- components/navg.php
+++ components/navg.php
@@ -3,39 +3,39 @@
             <ul>
                 <li>
                     <a href="#">
-                        <!-- <img src="images/apclogo.png" class="logo"> -->
-                        <div class="title" style="color:black;">APC Guidance Office </div>
+                        <img src="images/apclogo.png" class="apclogo">
+                        <div class="title" style="color:#FFD700;">APC Guidance Office </div>
                     </a>
                 </li>
                 <li>
                     <a href="guidance.php">
                         <i class="fas fa-th-large"></i>
-                        <div class="title" style="color:black;">Appointments</div>
+                        <div class="title" style="color:#FFD700;">Appointments</div>
                     </a>
                 </li>
                 <li>
                     <a href="guidanceAppoint.php">
                     <i class="fa fa-list-alt "></i>
-                        <div class="title" style="color:black;">Scheduling</div>
+                        <div class="title" style="color:#FFD700;">Scheduling</div>
                     </a>
                 </li>
                 <li>
                     <a href="guidanceInq.php">
                         <i class="fa fa-address-book"></i>
-                        <div class="title" style="color:black;">Inquiries</div>
+                        <div class="title" style="color:#FFD700;">Inquiries</div>
                     </a>
                 </li>
                 <li>
                     <a href="guidanceBlog.php">
                     <i class="fa fa-pencil"></i>
-                        <div class="title" style="color:black;">Blog Post</div>
+                        <div class="title" style="color:#FFD700;">Blog Post</div>
                     </a>
                 </li>
                 
                 <li>
                     <a href="logout.php">
                         <i class="fa fa-address-book"></i>
-                        <div class="title" style="color:black;">Logout</div>
+                        <div class="title" style="color:#FFD700;">Logout</div>
                     </a>
                 </li>
             </ul>
